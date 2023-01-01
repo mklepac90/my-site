0cf5847b7c2e3abc52f891e89e5d55632d8eedcc
Michael Klepac <67074804+mklepac90@users.noreply.github.com>
2023-01-01 11:32:08 -0500

    Update siteConfig.js

--- src/lib/siteConfig.js
+++ src/lib/siteConfig.js
@@ -1,8 +1,9 @@
-export const SITE_URL = 'https://swyxkit.netlify.app';
+export const SITE_URL = 'https://michaelklepac.com';
 export const APPROVED_POSTERS_GH_USERNAME = ['mklepac90'];
 export const GH_USER_REPO = 'mklepac90/my-site'; // used for pulling github issues and offering comments
-export const SITE_TITLE = 'SwyxKit';
-export const SITE_DESCRIPTION = "swyx's default SvelteKit + Tailwind starter";
+export const SITE_TITLE = 'Michael Klepac';
+export const SITE_DESCRIPTION =
+	"Michael Klepac's personal website full of random thoughts and cool things!";
 export const DEFAULT_OG_IMAGE =
 	'https://user-images.githubusercontent.com/6764957/147861359-3ad9438f-41d1-47c8-aa05-95c7d18497f0.png';
 export const MY_TWITTER_HANDLE = 'swyx';
